19b7e60d54e677a798952a3b370db32e802b85e6
Tyrone Damasceno <tyronedamasceno@gmail.com>
2018-10-03 15:51:57 -0300

    Removing mocked instancies

--- src/main/java/com/example/demo/UrlShortenerApplication.java
+++ src/main/java/com/example/demo/UrlShortenerApplication.java
@@ -1,8 +1,5 @@
 package com.example.demo;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,18 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UrlShortenerApplication implements CommandLineRunner{
 
-	@Autowired
-	private URLRepository repository;
-	
 	public static void main(String[] args) {
 		SpringApplication.run(UrlShortenerApplication.class, args);	
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		URL url1 = new URL(null, "www.facebook.com", "00001");
-		URL url2 = new URL(null, "www.youtube.com", "00002");
-		
-		repository.saveAll(Arrays.asList(url1, url2));
+		// TODO Auto-generated method stub	
 	}
 }
